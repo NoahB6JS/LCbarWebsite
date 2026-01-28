9dbe80037c8206eb4e7be112739934a348653e16
NoahB6JS <noah.js.b6@gmail.com>
2026-01-28 15:40:46 +0000

    quote button overlay

---
+++ LeonesBarHireWeb/javascript/main.js
@@ -0,0 +1,9 @@
+function openQuote() {
+  document.getElementById("quotePopup").style.display = "block";
+  document.getElementById("quoteOverlay").style.display = "block";
+}
+
+function closeQuote() {
+  document.getElementById("quotePopup").style.display = "none";
+  document.getElementById("quoteOverlay").style.display = "none";
+}
