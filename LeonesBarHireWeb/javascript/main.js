a3d00d8408fc547d744f9b644d2ae6c67e873dca
Noah John Samuel Blanchard <noah.js.b6@gmail.com>
2026-03-02 17:55:57 +0000

    Comments

--- LeonesBarHireWeb/javascript/main.js
+++ LeonesBarHireWeb/javascript/main.js
@@ -1,47 +1,37 @@
 
 
-//CODE FOR OPENING AND CLOSING QUOTE TAB
-
-// POPUP 1 (Email Form)
+// POPUP 1 OPENING AND CLOSING (Quote Form)
 function openQuote() {
   document.getElementById("quoteOverlay").style.display = "block";
   document.getElementById("quotePopup").style.display = "block";
 }
-
+//CLOSING THE QUOTE FORM
 function closeQuote() {
-  document.getElementById("quoteOverlay").style.display = "none";
-  document.getElementById("quotePopup").style.display = "none";
+  document.getElementById("quoteOverlay").style.display ="none";
+  document.getElementById("quotePopup").style.display="none";
 }
 
 
-// POPUP 2 (Instant Planner)
+// POPUP 2 (pRICE Planner)
 function openQuotePlanner() {
-  document.getElementById("quoteOverlay1").style.display = "block";
-  document.getElementById("quotePopup1").style.display = "block";
+  document.getElementById("quoteOverlay1").style.display ="block";
+  document.getElementById("quotePopup1").style.display ="block";
 }
 
+//CLOSING THE QUOTE FORM
 function closeQuotePlanner() {
   document.getElementById("quoteOverlay1").style.display = "none";
   document.getElementById("quotePopup1").style.display = "none";
 }
 
-
 //CODE FOR CALCULATING PRICE FOR THE QUOTES
-
 document.getElementById("calculate").addEventListener("click", function () {
 
+  //DEFINING VAR FOR CALCULATING THE PRICE
     let guests = parseInt(document.getElementById("guests").value);
     let foodCostPerPerson = parseFloat(document.getElementById("foodPackage").value);
-
-    // Base food cost
     let total = guests * foodCostPerPerson;
-
-    // Add-ons
     let addons = document.querySelectorAll(".addon:checked");
-    addons.forEach(addon => {
-        total += parseFloat(addon.value);
-    });
-
-    // Update price in the DOM
+    addons.forEach(addon => {total += parseFloat(addon.value);});
     document.getElementById("totalPrice").textContent = total.toFixed(2);
 });
